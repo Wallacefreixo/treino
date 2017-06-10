e2d7f8431dda0e3429c6f2d33952200d136bd24a
Wallace Freixo <wallace20006@hotmail.com>
2017-06-10 15:12:41 -0700

    Testando

---
+++ git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name "Wallace Freixo"
+git config --global user.email wallace20006@hotmail.com
+git add *
+git commit -m "Testando"
+git push
---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read file
+mkdir $file
+touch $file.txt
+cp $file.txt $file
